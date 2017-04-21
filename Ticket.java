860ecc6f3f02439d6638d2974f5a2a210b29ae65
Sasha Fomina <Sasha Fomina>
2017-04-21 11:02:19 -0400

    stuff

---
+++ Ticket.java
@@ -0,0 +1,15 @@
+public class Ticket implements Comparable {
+    private int _priority;
+    private String _descrip;
+    private String _name;
+    private boolean _resolved;
+    private String _solution;
+    private final int _ID;
+
+    
+    public Ticket(int priority, String descrip, String name, boolean _resolved,String _solution, int _ID ){
+	_priority = priority;
+	_description = 
+	
+    }
+}
