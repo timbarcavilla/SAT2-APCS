3a6930065d2f223102c8101fa5c3911fdcc46b28
timbarcavilla <zwang6@stuy.edu>
2017-04-22 12:36:14 -0400

    finished constructor, deleted solution (idt we need it), accessors added

--- Ticket.java
+++ Ticket.java
@@ -3,13 +3,29 @@ public class Ticket implements Comparable {
     private String _descrip;
     private String _name;
     private boolean _resolved;
-    private String _solution;
-    private final int _ID;
+    private final int _id;
 
     
-    public Ticket(int priority, String descrip, String name, boolean _resolved,String _solution, int _ID ){
+    public Ticket(int priority, String descrip, String name, boolean _resolved, int id ){
 	_priority = priority;
-	_description = 
+	_description = descrip;
+	_name = name;
+	_resolved = false;
+	_id = id;
 	
     }
+
+    //-------------ACCESSORS-------------
+
+    public int getPriority(){ return _priority; }
+    public String getDescrip(){ return _descrip; }
+    public String getName(){ return _name; }
+    public boolean isResolved(){ return _resolved; }
+    public int getID(){ return _id; }
+    
+    //-----------------------------------
+
+    //-------------MUTATORS--------------
+
+    
 }
